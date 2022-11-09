380fadfa83e5a7f6168f989146764f354cf60f7d
CyujunC <112966961+CyujunC@users.noreply.github.com>
2022-11-09 10:43:37 +0900

    Add files via upload

--- project/pj1.c
+++ project/pj1.c
@@ -4,29 +4,23 @@
  * 숫자: 5자리
  * 예: 57256
 */
+
+// 숙제1. 값을 직접 입력 (scanf 사용)
 int main(void) {
-    int num = 57256;
+    int num = 46324;
     int total = 0;
     
-    while(total>=0) {
-        if(num / 10000 != 0) {    
-            printf(num % 10000);
-        }
-        if(num / 1000 != 0) {
-            printf(num % 1000);
-        }
-        if(num / 100 != 0) {
-            printf(num % 100);
-        }
-        if(num / 10 != 0) {
-            printf(num % 10);
-        }
-        if(num / 1 != 0) {
-            printf(num % 1);
-        }
-        total += num;
-    
-    // 5 + 7 + 2 + 5 + 6 = 25
-    printf("총합 %d\n", total);  // 25출력
-}
+    // 46324 → 10  몫: 4632  나머지: 4
+    // 4632 → 10  몫: 463  나머지: 2
+    // 463 → 10  몫: 46  나머지: 3
+    // 46 → 10  몫: 4  나머지: 6
+    // 4 → 10  몫: 0  나머지: 4
+
+    // 숙제2. 자리수 제한 없는 상황에서도 똑같이 동작!
+    for(int i=1; i<=5; i++) {
+        total += (num % 10);  //  총합
+        num = num / 10;  // 다음 숫자(- 1자리;)
+        
+    }
+    printf("총합 %d\n", total);  
 }
